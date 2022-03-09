ca9edc318a98b215c243f6c9f3c1d5c68670c108
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-03-09 18:35:24 -0500

    Create 2022-03-09_Estado_de _Red_No_Electricas.sql

---
+++ 2022-03-09_Estado_de _Red_No_Electricas.sql
@@ -0,0 +1,151 @@
+WITH
+  AVERIAS AS(
+  SELECT
+   DATE_ADD(FECHA_DE_INICIO, INTERVAL 6 HOUR) AS FECHAINICIO,
+  FECHA_DE_FINAL,
+    NODO,
+    EXTRACT( MONTH
+    FROM
+      FECHA_DE_INICIO) AS MES,
+      EXTRACT( HOUR FROM FECHA_DE_INICIO) AS HORA,
+    Ticket_ID,
+    Clientes_Afectados,
+    DIFERENCIA_FECHAS_HORAS,
+    SISTEMA
+  FROM
+    `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-02-07_CR_AVERIAS_NODOS_2020-12_A_2021-12_D`
+  WHERE
+    Ticket_Type IS NOT NULL
+    AND (Ticket_Type ="Averia Planta Externa"
+      OR Ticket_Type ="Averia Fibra")
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=22 AND DIFERENCIA_FECHAS_HORAS <=8)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=23 AND DIFERENCIA_FECHAS_HORAS <=7)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=0 AND DIFERENCIA_FECHAS_HORAS <=6)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=1 AND DIFERENCIA_FECHAS_HORAS <=5)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=2 AND DIFERENCIA_FECHAS_HORAS <=4)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=3 AND DIFERENCIA_FECHAS_HORAS <=3)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=4 AND DIFERENCIA_FECHAS_HORAS <=2)
+      AND NOT (EXTRACT( HOUR FROM FECHA_DE_INICIO)=5 AND DIFERENCIA_FECHAS_HORAS <=1)
+      AND DIFERENCIA_FECHAS_HORAS IS NOT NULL
+      AND (SISTEMA="Fibra Optica" OR SISTEMA="Planta Externa" OR SISTEMA ="Sin Intervención Técnica")
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9 ),
+  GEOUSUARIOS AS (
+  SELECT
+    ID_NODO,
+    PROVINCIA,
+    Cant__n,
+    DISTRITO,
+    Latitud,
+    Longitud,
+    CANTIDAD_DE_USUARIOS
+  FROM
+    `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-02-28_ACCESO_GEO_D`
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7),
+  USUARIOSAVERIAS AS(
+  SELECT
+    a.NODO,
+    a.FECHAINICIO,
+    a.FECHA_DE_FINAL,
+    a.MES,
+    a.Ticket_ID,
+    A.Clientes_Afectados,
+    a.DIFERENCIA_FECHAS_HORAS,
+    a.SISTEMA,
+    g.ID_NODO,
+    g.PROVINCIA,
+    g.Cant__n,
+    g.DISTRITO,
+    g.CANTIDAD_DE_USUARIOS,
+    g.Latitud,
+    g.Longitud
+  FROM
+    AVERIAS a
+  LEFT JOIN
+    GEOUSUARIOS g
+  ON
+    a.NODO = g.ID_NODO
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15),
+  TABLAFINAL AS(
+  SELECT
+    DISTINCT MES,
+    Ticket_iD,
+    FECHAINICIO,
+    FECHA_DE_FINAL,
+    NODO,
+    PROVINCIA,
+    Cant__n,
+    DISTRITO,
+    SISTEMA,
+    CANTIDAD_DE_USUARIOS,
+    Clientes_Afectados,
+    DIFERENCIA_FECHAS_HORAS,
+    latitud,
+    longitud
+  FROM
+    USUARIOSAVERIAS
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14)
+SELECT
+  Ticket_ID,
+  MES,
+  FECHAINICIO,
+  FECHA_DE_FINAL,
+  Latitud,
+  longitud,
+  SISTEMA,
+  NODO,
+  PROVINCIA,
+  Cant__n,
+  DISTRITO,
+  CANTIDAD_DE_USUARIOS,
+  Clientes_Afectados,
+  DATE_DIFF( FECHA_DE_FINAL,FECHAINICIO, HOUR) AS DIFERENCIA_HORAS,
+  DATE_DIFF( FECHA_DE_FINAL,FECHAINICIO, MINUTE) AS DIFERENCIA_MINUTOS
+FROM
+  TABLAFINAL
+ORDER BY
+  MES
